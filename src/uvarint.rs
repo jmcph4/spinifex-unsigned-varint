09e59677d44b4424ac1a673d1e2132406318acf0
jmcph4 <jmcph4.github@gmail.com>
2020-03-24 18:08:26 +1000

    Remove `BitVec` from definition of `UVarInt` type
    This commit redefines the `UVarInt` type to remove the need for the
    `BitVec` type.
    
    This commit also adds a constructor for the `UVarInt` type.

--- src/uvarint.rs
+++ src/uvarint.rs
@@ -1,9 +1,22 @@
-#![allow(dead_code)]
-extern crate bitvec;
-use bitvec::vec::BitVec;
+use std::convert::TryInto;
+
+pub const BITS_PER_BYTE: usize = 8;
+pub const MAX_UVARINT_NUM_BYTES: usize = 9;
+
+pub enum EncodeError {
+    OutOfRange
+}
 
 pub struct UVarInt {
-    num: u128,
-    bits: BitVec
+    num: u128
+}
+
+impl UVarInt {
+    pub fn new(num: u128) -> Self {
+        UVarInt {
+            num: num
+        }
+    }
+
 }
 
